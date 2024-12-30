33cc4daca31638072f29f3322a94c4801b1544c7
Kevin Kreulen <16885904+kreulenk@users.noreply.github.com>
2024-12-30 09:25:24 -0600

    Initial cobra code added for cli interaction

---
+++ cmd/root.go
@@ -0,0 +1,28 @@
+package cmd
+
+import (
+	"os"
+
+	"github.com/spf13/cobra"
+)
+
+// rootCmd represents the base command when called without any subcommands
+var rootCmd = &cobra.Command{
+	Use:   "mtui <host-name>",
+	Short: "A MongoDB Terminal User Interface",
+	Long: `mtui is a MongoDB Terminal User Interface that allows you to interact with MongoDB databases from the CLI
+in a more intuitive way.`,
+}
+
+// Execute adds all child commands to the root command and sets flags appropriately.
+// This is called by main.main(). It only needs to happen once to the rootCmd.
+func Execute() {
+	err := rootCmd.Execute()
+	if err != nil {
+		os.Exit(1)
+	}
+}
+
+func init() {
+	//rootCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+}
---
+++ main.go
@@ -0,0 +1,7 @@
+package main
+
+import "mtui/cmd"
+
+func main() {
+	cmd.Execute()
+}
